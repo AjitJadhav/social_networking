5ae17dfa1f1794d7faf618e6898d85cc70662695
Ajit <ajit@grepruby.com>
2014-04-24 17:45:07 +0530

    image uploader path fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,7 +16,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "/home/rails/railsProject/social_networking/public/uploads/tmp/images"
+    "/public/uploads/tmp/images"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
